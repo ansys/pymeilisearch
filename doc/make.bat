dd6588d0b016d0ec0409e9975031e26ae612090b
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2023-03-09 09:24:16 +0100

    doc: Add the pdf build

--- doc/make.bat
+++ doc/make.bat
@@ -11,7 +11,7 @@ set SOURCEDIR=source
 set BUILDDIR=_build
 
 if "%1" == "" goto help
-if "%1" == "clean" goto clean
+if "%1" == "pdf" goto pdf
 
 %SPHINXBUILD% >NUL 2>NUL
 if errorlevel 9009 (
@@ -29,13 +29,14 @@ if errorlevel 9009 (
 %SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
 goto end
 
-:clean
-rmdir /s /q %BUILDDIR% > /NUL 2>&1 
-for /d /r %SOURCEDIR% %%d in (_autosummary) do @if exist "%%d" rmdir /s /q "%%d"
-goto end
-
 :help
 %SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
 
+:pdf
+	%SPHINXBUILD% -M latex %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+	cd "%BUILDDIR%\latex"
+	for %%f in (*.tex) do (
+	pdflatex "%%f" --interaction=nonstopmode)
+	
 :end
 popd
