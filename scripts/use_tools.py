85607a28d96e68370c6f812021ca64cbcba2c871
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2023-03-16 18:39:47 +0100

    test the api

---
+++ scripts/use_tools.py
@@ -0,0 +1,12 @@
+from ansys.tools.meilisearch.all_doc_indexer import DocsAllPublic
+from ansys.tools.meilisearch.client import MeilisearchClient
+from ansys.tools.meilisearch.scrapper import WebScraper
+
+# Scrape a single url will desired index
+client = MeilisearchClient()
+scrapper = WebScraper(client.meilisearch_host_url, client.meilisearch_api_key)
+n_hits = scrapper.scrape_url("https://sphinxdocs.ansys.com/", "testing-index")
+print(n_hits)
+
+all_doc = DocsAllPublic(client, "testing-all")
+all_doc.add_all_public_doc(["testing"])
