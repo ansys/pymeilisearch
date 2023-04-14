6e1cb24c0f845cca17ac6fd865f66ffdf9f4e877
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2023-04-14 16:02:00 +0200

    Fix: get total num of docs

--- tests/test_create_indexes.py
+++ tests/test_create_indexes.py
@@ -85,7 +85,9 @@ def test_is_sphinx(url):
 
 
 def test_temp_index_swapping(meilisearch_client):
-    test_url = {"ansys/ansys-sphinx-theme": "https://sphinxdocs.ansys.com"}
+    test_url = {
+        "ansys/ansys-sphinx-theme": "https://sphinxdocs.ansys.com",
+    }
     create_sphinx_indexes(
         test_url,
         meilisearch_client.meilisearch_host_url,
@@ -94,3 +96,4 @@ def test_temp_index_swapping(meilisearch_client):
     stats = meilisearch_client.client.get_all_stats()
     index_uids = list(stats["indexes"].keys())
     assert "ansys-ansys-sphinx-theme-sphinx-docs" in index_uids
+    print()
