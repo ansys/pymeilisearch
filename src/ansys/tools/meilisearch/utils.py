277976bf43a1f125fbe882d0115cb0307b29f59d
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2023-04-14 11:34:02 +0200

    Fix: timeout error

--- src/ansys/tools/meilisearch/utils.py
+++ src/ansys/tools/meilisearch/utils.py
@@ -8,6 +8,14 @@ import requests
 from ansys.tools.meilisearch.client import MeilisearchClient
 
 
+class MeiliSearchApiError(Exception):
+    pass
+
+
+class MeiliSearchApiTimeout(Exception):
+    pass
+
+
 class MeilisearchUtils:
     """
     A collection of utility functions for working with MeiliSearch.
@@ -62,31 +70,34 @@ class MeilisearchUtils:
 
         return documents
 
-    def _wait_task(self, task_uid: int, timeout: float = 10.0) -> None:
+    def _wait_task(self, task_uid: int, timeout: float = 60.0) -> None:
         """
         Wait until a task is complete.
 
-        If a task exceeds the timeout, raise a TimeoutError.
+        If a task exceeds the timeout, raise a MeiliSearchApiTimeout.
 
         Parameters
         ----------
         task_uid : int
             Task UID.
         timeout : float
-            Timeout value in seconds. Defaults to 10.0.
+            Timeout value in seconds. Defaults to 60.0.
 
         Raises
         ------
-        TimeoutError
-            Raised when the ``timeout`` is exceed.
+        MeiliSearchApiTimeout
+            Raised when the timeout is exceeded.
         RuntimeError
-            Raised when the status of ``task`` failed.
+            Raised when the status of the task is failed.
         """
-        task_url = f"{self._api.meilisearch_host_url}/tasks/{task_uid}"
+        task_url = f"{self.meilisearch_host_url}/tasks/{task_uid}"
         timeout_time = time.time() + timeout
         while time.time() < timeout_time:
-            response = requests.get(task_url, headers=self._api.headers)
-            jresp = response.json()
+            response = requests.get(task_url, headers=self.headers)
+            try:
+                jresp = response.json()
+            except json.decoder.JSONDecodeError as e:
+                raise MeiliSearchApiError(f"Failed to decode response: {e}") from e
             if jresp["status"] == "failed":
                 if "error" in jresp:
                     msg = jresp["error"]["message"]
@@ -99,4 +110,4 @@ class MeilisearchUtils:
             else:
                 time.sleep(1)
         if time.time() > timeout_time:
-            raise TimeoutError(f"Exceeded timeout {timeout}")
+            raise MeiliSearchApiTimeout(f"Timeout exceeded: {timeout} seconds")
