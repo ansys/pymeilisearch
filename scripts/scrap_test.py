196f964373c467fcdacf97b59fd33d7754a51b0b
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2023-04-14 16:54:59 +0200

    Fix: typo

--- scripts/scrap_test.py
+++ scripts/scrap_test.py
@@ -4,7 +4,7 @@ from ansys.tools.meilisearch.client import MeilisearchClient
 from ansys.tools.meilisearch.create_indexes import create_sphinx_indexes, get_sphinx_urls
 
 
-def create_sphinx_indexes(urls, client):
+def create_filtered_sphinx_indexes(urls, client):
     filtered_sphinx_urls = get_sphinx_urls(urls)
     create_sphinx_indexes(
         filtered_sphinx_urls, client.meilisearch_host_url, client.meilisearch_api_key
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "function_name",
-        choices=["create_sphinx_indexes"],
+        choices=["create_filtered_sphinx_indexes"],
         help="Name of function to run",
     )
     args = parser.parse_args()
@@ -26,6 +26,6 @@ if __name__ == "__main__":
     }
 
     if args.function_name == "create_sphinx_indexes":
-        create_sphinx_indexes(urls, client)
+        create_filtered_sphinx_indexes(urls, client)
     else:
         raise ValueError("Invalid function name: {}".format(args.function_name))
