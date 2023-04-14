4ff4965cfdb54d2a583e175cb1a264cd7cb6549e
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2023-04-14 16:51:11 +0200

    Fix: test real time

---
+++ scripts/scrap_test.py
@@ -0,0 +1,31 @@
+import argparse
+
+from ansys.tools.meilisearch.client import MeilisearchClient
+from ansys.tools.meilisearch.create_indexes import create_sphinx_indexes, get_sphinx_urls
+
+
+def create_sphinx_indexes(urls, client):
+    filtered_sphinx_urls = get_sphinx_urls(urls)
+    create_sphinx_indexes(
+        filtered_sphinx_urls, client.meilisearch_host_url, client.meilisearch_api_key
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "function_name",
+        choices=["create_sphinx_indexes"],
+        help="Name of function to run",
+    )
+    args = parser.parse_args()
+
+    client = MeilisearchClient()
+    urls = {
+        "pyansys/pyaedt": "https://aedt.docs.pyansys.com",
+    }
+
+    if args.function_name == "create_sphinx_indexes":
+        create_sphinx_indexes(urls, client)
+    else:
+        raise ValueError("Invalid function name: {}".format(args.function_name))
