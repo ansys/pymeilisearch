22364bfc87f3a5cd85743e0e1a8803dc8f8a3131
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2023-04-14 14:50:33 +0200

    Fix: fix typo

--- src/ansys/tools/meilisearch/utils.py
+++ src/ansys/tools/meilisearch/utils.py
@@ -90,10 +90,10 @@ class MeilisearchUtils:
         RuntimeError
             Raised when the status of the task is failed.
         """
-        task_url = f"{self.meilisearch_host_url}/tasks/{task_uid}"
+        task_url = f"{self._api.meilisearch_host_url}/tasks/{task_uid}"
         timeout_time = time.time() + timeout
         while time.time() < timeout_time:
-            response = requests.get(task_url, headers=self.headers)
+            response = requests.get(task_url, headers=self._headers)
             try:
                 jresp = response.json()
             except json.decoder.JSONDecodeError as e:
