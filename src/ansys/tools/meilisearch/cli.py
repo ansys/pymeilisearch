a66245505e837d05b065b7aa70078d27e3863df9
Jorge Martinez Garrido <jorge.martinezgarrido@ansys.com>
2023-05-04 16:19:16 +0200

    FEAT: add layout for CLI

--- src/ansys/tools/meilisearch/cli.py
+++ src/ansys/tools/meilisearch/cli.py
@@ -1,20 +1,6 @@
 import click
 
-from ansys.tools.meilisearch import __version__, scrap_local
-
-
-def scrape_page(template, path):
-    """scrape the page based on given template.
-
-    Parameters
-    ----------
-    template : str
-        Name of the template to be used as basis for the project
-    path : str
-        Path to HTML files to be uploaded
-
-    """
-    scrap_local(template, path)
+from ansys.tools.meilisearch import __version__
 
 
 @click.group()
@@ -30,14 +16,13 @@ def main():
 @click.argument('location')
 def upload(template, index, source, location):
     """Upload files or a website using the specified template and index."""
+
     if source == 'html':
-        click.echo(f"Uploading HTML files using template '{template}' and index '{index}':")
-        click.echo(f"- Location: {location}")
-        # Add your upload code for HTML files here.
+        raise NotImplementedError("The {source} argument is not implemented yet.")
+
     elif source == 'url':
-        click.echo(f"Uploading website using template '{template}' and index '{index}':")
-        click.echo(f"- URL: {location}")
-        # Add your upload code for websites here.
+        raise NotImplementedError("The {source} argument is not implemented yet.")
+
     else:
         click.echo(f"Invalid source: {source}. Must be 'html' or 'url'.")
 
