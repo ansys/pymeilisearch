7fe62fcb0927077a4ee960bb08a081060df23fc0
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2023-05-17 09:31:36 +0200

    Fix: fix the text removing the actions

--- tests/test_create_indexes.py
+++ tests/test_create_indexes.py
@@ -62,7 +62,6 @@ def public_urls():
     return {
         "pyansys/dev-guide ": "https://dev.docs.pyansys.com",
         "pyansys/pyansys": "https://docs.pyansys.com",
-        "pyansys/actions": "https://actions.docs.pyansys.com",
         "pyansys/pyseascape": "https://seascape.docs.pyansys.com",
     }
 
