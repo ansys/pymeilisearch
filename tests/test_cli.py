a26d258fe68c70ef4d95891a9301e6dd9dd89f5d
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2023-07-31 16:40:12 +0200

    FIX: cli tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,18 +1,22 @@
 from click.testing import CliRunner
+import pytest
 
 from ansys.tools.meilisearch.cli import main
 
-runner = CliRunner()
+
+@pytest.fixture
+def runner(scope="module"):
+    return CliRunner()
 
 
-def test_version_command():
+def test_version_command(runner):
     # Test the 'version' command
     result = runner.invoke(main, ["version"])
     assert result.exit_code == 0
     assert "PyMeilisearch" in result.output
 
 
-def test_upload_command_invalid_source(meilisearch_client):
+def test_upload_command_invalid_source(meilisearch_client, runner):
     # Test the 'upload' command with an invalid source option
     result = runner.invoke(
         main,
